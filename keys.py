2a486bd30c9190b5093f4f21a67db578d6b99c19
oblvisheee <molniya213y@gmail.com>
2024-02-03 20:26:46 +0300

    fixes

--- keys.py
+++ keys.py
@@ -3,13 +3,18 @@ from Crypto.PublicKey import RSA, ECC
 import hash_generator
 def generate_private_key_rsa(key_size=2048) -> str:
     key = RSA.generate(key_size)
-    private_key = key.export_key()
-    return str(private_key)
-
+    private_key = key.export_key().decode('utf-8')
+    return private_key + '\n'
 def generate_private_key_ecc(curve='P-256') -> str:
+    """l\
+    Supported curves:
+    - 'P-256'
+    - 'P-384'
+    - 'P-521'
+    """
     key = ECC.generate(curve=curve)
-    private_key = key.export_key()
-    return private_key
+    private_key = key.export_key(format='PEM')
+    return str(private_key)
 
 def generate_public_key(private_key, hash_type: str="sha256"):
     """

b83abb8e212db7a044e65df37ba716ea54c910d2
oblivisheee <molniya213y@gmail.com>
2024-02-17 18:07:53 +0300

    fixes.

--- encrypt.py
+++ encrypt.py
@@ -1,9 +1,9 @@
 import os
 from Crypto.Cipher import AES
 from Crypto.Random import get_random_bytes
-
+import base64
 class AESCipher:
-    def __init__(self, key, mode=AES.MODE_GCM):
+    def __init__(self, key, mode):
         self.key = key
         self.mode = mode
 
@@ -19,8 +19,20 @@ class AESCipher:
         plaintext = cipher.decrypt_and_verify(ciphertext, tag)
         return plaintext.decode('utf-8')
 
+    
 def generate_key(key_size=16):
     return get_random_bytes(key_size)
 
+class Base64Cipher:
+    def __init__(self, key):
+        self.key = key
 
+    def encrypt(self, plaintext):
+        if isinstance(plaintext, str):
+            plaintext = plaintext.encode('utf-8')
+        ciphertext = base64.b64encode(plaintext)
+        return ciphertext
 
+    def decrypt(self, ciphertext):
+        plaintext = base64.b64decode(ciphertext)
+        return plaintext.decode('utf-8')
